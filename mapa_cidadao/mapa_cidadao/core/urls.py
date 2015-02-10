214250788ed7c9cdaf53b8870e735fad54a5585d
Marcelo Tambalo <marcelo.zokis@gmail.com>
2015-02-09 22:56:20 -0200

    adiciona url de contato e sobre

--- mapa_cidadao/mapa_cidadao/core/urls.py
+++ mapa_cidadao/mapa_cidadao/core/urls.py
@@ -9,6 +9,8 @@ from mapa_cidadao.core.models import Ocorrencia
 urlpatterns = patterns(
     '',
     url(r'^$', 'mapa_cidadao.core.views.index', name='index'),
+    url(r'^contato/$', 'mapa_cidadao.core.views.contact', name='contact'),
+    url(r'^sobre/$', 'mapa_cidadao.core.views.about', name='about'),
     url(r'^get_current_geom/$', 'mapa_cidadao.core.views.get_current_geom', name='get_current_geom'),
     url(r'^ocorrencia/nova/$', 'mapa_cidadao.core.views.ocorrencia_crud', name='ocorrencia_crud'),
     url(r'^ocorrencia/(?P<pk>\d+)/$', 'mapa_cidadao.core.views.ocorrencia_crud', name='ocorrencia_crud'),
