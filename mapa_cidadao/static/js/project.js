335e8d841add19bb7617502cdd1b71dc9ddf0f77
Marcelo Fonseca Tambalo <marcelo.zokis@gmail.com>
2015-02-12 20:29:15 -0200

    arruma mover

--- mapa_cidadao/static/js/project.js
+++ mapa_cidadao/static/js/project.js
@@ -1,3 +1,84 @@
+if(typeof $.fn.mover_v == "undefined") {
+    $.fn.extend({
+        mover_v: function(parent) {
+            return $(this).each(function() {
+                var body_padding = parseInt($('body').css('padding-top'));
+                var $el = $(parent);
+                $el.css('top', body_padding + 4);
+                var dragging = false;
+                var start_y = 0;
+                var start_t = 0;
+                $el.mousedown(function(ev) {
+                    dragging = true;
+                    start_y = ev.clientY;
+                    start_t = $el.css('top');
+                });
+                $(window).mousemove(function(ev) {
+                    ev.preventDefault();
+                    if (dragging) {
+                        // calculate new top
+                        var new_top = parseInt(start_t) + (ev.clientY - start_y);
+                        //stay in parent
+                        var max_top = $(window).height()-$el.height();
+
+                        if(new_top > max_top){
+                            new_top = max_top;
+                        }
+                        if(new_top < body_padding){
+                            new_top = body_padding;
+                        }
+
+                        $el.css('top', new_top);
+                    }
+                }).mouseup(function() {
+                    dragging = false;
+                });
+            });
+        }
+    });
+}
+
+if(typeof $.fn.mover_h == "undefined") {
+    $.fn.extend({
+        mover_h: function(parent) {
+            return $(this).each(function() {
+                var $el = $(parent);
+                $el.css('left', 45);
+                var dragging = false;
+                var start_x = 0;
+                var start_t = 0;
+                $el.mousedown(function(ev) {
+                    dragging = true;
+                    start_x = ev.clientX;
+                    start_t = $el.css('left');
+                });
+                $(window).mousemove(function(ev) {
+                    ev.preventDefault();
+                    if (dragging) {
+                        // calculate new left
+                        var new_left = parseInt(start_t) + (ev.clientX - start_x);
+                        
+                        //stay in parent
+                        var max_left =  $( window ).width()-$el.width();
+                        
+                        if(new_left > max_left){
+                            new_left = max_left;
+                        }
+                        if(new_left < 0){
+                            new_left = 0;
+                        }
+
+                        $el.css('left', new_left );
+                    }
+                }).mouseup(function() {
+                    dragging = false;
+                });
+            });
+        }
+    });
+}
+
+
 $(document).ready(function() {
   // $('select').material_select();
   $('.datepicker').pickadate({
