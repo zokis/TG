f354d2064c11db5f78368eda5f8a901670457b6f
Marcelo Tambalo <marcelo.zokis@gmail.com>
2015-05-29 17:20:50 -0300

    ruim

--- mapa_cidadao/mapa_cidadao/core/views.py
+++ mapa_cidadao/mapa_cidadao/core/views.py
@@ -50,7 +50,7 @@ class MobTemplateMixin(object):
         return [self.template_name]
 
 
-def load_ocorrencias(request, x0=None, y0=None, x1=None, y1=None):
+def load_ocorrenciasx(request, x0=None, y0=None, x1=None, y1=None):
     bbox = Polygon.from_bbox([x0, y0, x1, y1]) if x0 else None
     geom = get_geom_from_cache()
     ocorrencias = Ocorrencia.objects.filter_by_geom_and_bbox(geom, bbox)
@@ -71,6 +71,43 @@ def load_ocorrencias(request, x0=None, y0=None, x1=None, y1=None):
                 yield ',\n'
         yield '\n]'
     response = StreamingHttpResponse(flush(), content_type='application/json')
+    response['Cache-Control'] = 'max-age=0, no-cache, no-store'
+    return response
+
+
+def load_ocorrencias(request, x0=None, y0=None, x1=None, y1=None):
+    geom = get_geom_from_cache()
+    ocorrencias = Ocorrencia.objects.filter(ponto__intersects=geom)
+    if not get_user_agent(request).is_mobile:
+        ocorrencias = SearchForm(request.GET or None, queryset=ocorrencias).get_queryset()
+
+    # def flush():
+    #     yield '[\n'
+    #     for ocorrencia, last in is_last(ocorrencias):
+    #         yield dumps({
+    #             'wkt': filters.safe(ocorrencia.ponto.wkt),
+    #             'name': ocorrencia.titulo,
+    #             'description': filters.wordwrap(ocorrencia.descricao, 10),
+    #             'pk': ocorrencia.pk,
+    #             'style': ocorrencia.get_estilo()
+    #         })
+    #         if not last:
+    #             yield ',\n'
+    #     yield '\n]'
+    x = dumps(
+        [
+            {
+                'wkt': filters.safe(ocorrencia.ponto.wkt),
+                'name': ocorrencia.titulo,
+                'description': filters.wordwrap(ocorrencia.descricao, 10),
+                'pk': ocorrencia.pk,
+                'style': ocorrencia.get_estilo()
+            }
+            for ocorrencia in ocorrencias
+        ]
+    )
+
+    response = HttpResponse(x, content_type='application/json')
     response['Cache-Control'] = 'max-age=0, no-cache, no-store'
     return response
 
